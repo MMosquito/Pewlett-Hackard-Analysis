34877ecf1b03e159af19f5468f2b5b263f913ce4
MMosquito <melissa.mosquito@gmail.com>
2023-01-21 17:47:56 -0600

    Add files via upload

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/PH-EmployeeDB:postgres@PostgreSQL 11.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/PH-EmployeeDB:postgres@PostgreSQL 11.sql
@@ -1,4 +1,4 @@
---Creating tables for PH-EmployeeDB
+-- Creating tables for PH-EmployeeDB
 
 CREATE TABLE departments (
      dept_no VARCHAR NOT NULL,
@@ -53,43 +53,4 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (dept_no)
 );
 
--- List employees begin to retire
-SELECT first_name, last_name
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
---List birthdates from 1952
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01'AND '1952-12-31';
-
---List birthdates from 1953
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1953-01-01'AND '1953-12-31';
-
---List birthdates from 1954
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1954-01-01'AND '1954-12-31';
-
---List birthdates from 1955
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1955-01-01'AND '1955-12-31';
-
---Number of employees retiring
-SELECT COUNT (first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-	
---List retirement-eligible employees	
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-	
-SELECT * FROM retirement_info;	
+SELECT * FROM departments;
