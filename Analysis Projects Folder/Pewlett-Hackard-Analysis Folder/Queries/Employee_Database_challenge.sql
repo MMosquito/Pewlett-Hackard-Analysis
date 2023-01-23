2e309b4e4bdf4cd9bbefc35c0def371b1e63be69
MMosquito <melissa.mosquito@gmail.com>
2023-01-22 18:36:53 -0600

    Add files via upload

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -54,3 +54,54 @@ CREATE TABLE salaries (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, from_date)
 );
+
+--DELIVERABLE #1: The Number of Retiring Employees by Title
+--Create Retirement Titles table
+	SELECT employees.emp_no, 
+	employees.first_name, 
+  	employees.last_name,
+	titles.title,
+	titles.from_date,
+	titles.to_date
+	INTO retirement_titles
+	FROM employees
+	INNER JOIN titles
+	ON employees.emp_no = titles.emp_no
+	WHERE employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+	ORDER BY employees.emp_no;
+	
+--Use distinct on to retrieve first occurance
+	SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+	INTO unique_titles
+	FROM retirement_titles
+	WHERE to_date = '9999-01-01'
+	ORDER BY emp_no, to_date DESC;
+	
+--Number employees ready to retire by most recent job title
+	SELECT COUNT(unique_titles.emp_no),
+	(unique_titles.title)
+	INTO retiring_titles
+	FROM unique_titles
+	GROUP BY title
+	ORDER BY COUNT(title) DESC;
+	
+	--DELIVERABLE #2: The Employees Eligible for the Mentorship Program
+--Create quesry for Mentorship Eligibility 
+	SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+		employees.first_name,
+		employees.Last_name,
+		employees.birth_date,
+		dept_emp.from_date,
+		dept_emp.to_date,
+		titles.title
+	INTO mentorship_eligibility
+	FROM employees
+	LEFT OUTER JOIN dept_emp
+	ON (employees.emp_no = dept_emp.emp_no)
+	LEFT OUTER JOIN titles
+	ON (employees.emp_no = titles.emp_no)
+	WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	ORDER BY employees.emp_no ASC;
